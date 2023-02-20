8ed953999197ddcd7cfd84e5a2179d07ecee6859
Ali Homayouni <ali.sh.homayouni>
2023-02-20 15:44:23 +0100

    02202023

---
+++ test.py
@@ -0,0 +1,20 @@
+num = 2466
+num = str(num)
+outcom = ''
+_len = len(num)
+i = 0
+token = 'r5'
+token_index = 0
+while i < _len:
+    count = 1
+    pointer = i+1
+    while pointer < _len and num[i] == num[pointer]:
+        count += 1
+        pointer += 1
+    _token1 = token[token_index] if token_index < len(token) else ''
+    _token2 = token[token_index+1] if token_index+1 < len(token) else ''
+    outcom += str(count) + _token1 + num[i] + _token2
+    i += count
+    token_index += 2
+print(outcom + token[token_index:])
+
